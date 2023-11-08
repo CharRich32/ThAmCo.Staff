134d46190104e389998a9e055dfdc170cf577048
CHARLIE RICHARDSON <B1044146@live.tees.ac.uk>
2023-11-08 11:58:04 +0000

    Created a fake service with test to ensure it works as expected Created classes and files for Models and Services

---
+++ ThAmCo.Staff/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace ThAmCo.Staff.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public List<Product> Products { get; set; }
+        public DateTime RequestedDate { get; set; }
+    }
+}
---
+++ ThAmCo.Staff/Services/IOrdersService.cs
@@ -0,0 +1,10 @@
+﻿using ThAmCo.Staff.Models;
+
+namespace ThAmCo.Staff.Services
+{
+    public interface IOrdersService
+    {
+        public List<Order> GetOrdersAsync();
+
+    }
+}
